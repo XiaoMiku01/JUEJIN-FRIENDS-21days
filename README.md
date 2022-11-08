628915672a13ff30e7e24ece498794f7c125f4d7
XiaoMiku01 <loveava0612@outlook.com>
2022-11-08 21:10:42 +0800

    :tada: first commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # JUEJIN-FRIENDS-21days
-JUEJIN FRIENDS 好好生活计划 21天自动签到脚本
+
+JUEJIN FRIENDS 好好生活计划 21 天自动签到脚本
+
+## 使用方法
+
+1. clone 本项目到本地
+2. 安装依赖
+
+```bash
+pip3 install -r requirements.txt
+```
+
+3. 修改 `main.py` 文件中的 `cookie` 和 `content`
+
+    > `cookie` 获取方式：登录掘金官网 (https://juejin.cn/)
+    > `F12` 打开开发者工具，切换到 `控制台` 标签页，输入 `document.cookie`，复制输出的值
+
+4. 运行脚本
+
+```bash
+python3 main.py
+```
+
+## 说明
+
+1. 本项目仅供学习交流使用，不得用于商业用途
+2. 运行环境：`Python 3`
+3. 脚本第一次运行会自动发一贴，如果发送成功说明配置正常，后续每天 00:01 自动打卡
+4. 脚本需要保持运行，建议使用 `screen` 或 `tmux` 等工具
---
+++ main.py
@@ -0,0 +1,59 @@
+# Web端cookie
+cookie = """
+此行删除后填写你的cookie
+"""
+
+# 打卡内容
+content = "每日打卡"
+
+### 下面不用改
+import requests
+import logging, time
+
+logging.basicConfig(
+    format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s', level=logging.DEBUG
+)
+
+
+day_n = 0
+
+
+def day():
+    global day_n
+    api = "https://api.juejin.cn/content_api/v1/short_msg/publish"
+    headers = {
+        "cookie": cookie.strip(),
+        "content-type": "application/json",
+    }
+    json = {
+        "content": "[7163537723910225960#JUEJIN FRIENDS 好好生活计划#] " + content + " day" + str(day_n),
+        "sync_to_org": False,
+        "theme_id": "7163537723910225960",
+    }
+    retry_times = 5
+    while True:
+        try:
+            r = requests.post(api, headers=headers, json=json)
+            if r.status_code == 200 and r.json()['err_msg'] == 'success':
+                logging.info("day", day_n, "打卡成功")
+            else:
+                logging.error("day", day_n, "打卡失败", r.json())
+            break
+        except Exception as e:
+            logging.error("day", day_n, "打卡失败", e, "重试中")
+            retry_times -= 1
+            if retry_times == 0:
+                logging.error("day", day_n, "打卡失败", e, "重试次数超限，放弃")
+                break
+            time.sleep(5)
+    day_n += 1
+
+
+if __name__ == "__main__":
+    import schedule
+
+    day()  # 启动时先打卡一次
+    schedule.every().day.at("00:01").do(day, day_n)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
